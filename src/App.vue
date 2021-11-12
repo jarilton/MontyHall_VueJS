f909485c9e3520a2d8f5d0f8aaeec0623c046bf3
jarilton <jariltonpereira02@hotmail.com>
2021-11-12 17:49:32 -0300

    Implememtando o objeto de presente

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <template>
     <div id="app">
         <h1>Problema de Monty Hall</h1>
+        <Gift />
     </div>
 </template>
 
 <script>
+import Gift from './components/Gift.vue'
+
 export default {
+    name: 'App',
+    components: { Gift }
 
 }
 </script>
