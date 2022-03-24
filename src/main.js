12e4bb74ea3ba4b9015297ae5db0b6a0233ae891
jarilton <jariltonpereira02@hotmail.com>
2022-03-24 14:07:38 -0300

    adjusting main application

--- src/main.js
+++ src/main.js
@@ -2,9 +2,9 @@ import Vue from "vue";
 import App from './App'
 
 new Vue({
-    render: h => h(App)
-/*     el: "#app",
-    render(createElement) {
+    render: h => h(App),
+    el: "#app",
+    reRender(createElement) {
         return createElement(App)
-    }  */
+    } 
 }).$mount("#app")
